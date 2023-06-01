054939cfc2afd908d73b214768427476c75bf74a
Bivash43 <bivash43e@gmail.com>
2023-06-01 08:10:07 +0545

    Google Map API Key Integration

---
+++ GoogleMapIntegration/app/src/main/java/com/example/googlemapintegration/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.googlemapintegration;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+        map = googleMap;
+
+        LatLng kathmanduLocation = new LatLng(27.7172, 85.3240);
+
+        map.addMarker(new MarkerOptions().position(kathmanduLocation).title("Kathmandu"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(kathmanduLocation));
+
+    }
+}
