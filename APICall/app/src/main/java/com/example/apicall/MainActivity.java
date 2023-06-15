533bb3d4d0c01036479ebc26c154a7d9dc379dca
Bivash43 <bivash43e@gmail.com>
2023-06-15 07:52:12 +0545

    UI created to view JSON Objects

--- APICall/app/src/main/java/com/example/apicall/MainActivity.java
+++ APICall/app/src/main/java/com/example/apicall/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.apicall;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.DownloadManager;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -12,21 +11,33 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String API_URL = "https://jsonplaceholder.typicode.com/posts/1";
 
+    TextView tv1 , tv2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tv1 = findViewById(R.id.Text);
+        tv2 = findViewById(R.id.Text2);
+
+
         JsonObjectRequest jsonObject = new JsonObjectRequest(Request.Method.GET, API_URL, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-                Toast.makeText(MainActivity.this, "SUCCEESS", Toast.LENGTH_SHORT).show();
+                try {
+                    tv1.setText(response.get("title").toString());
+                    tv2.setText(response.get("body").toString());
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
             }
         },new Response.ErrorListener() {
             @Override
