89cd9cf97f429c83add63dfca87b97b42989d2cb
Bivash43 <bivash43e@gmail.com>
2023-06-16 07:47:52 +0545

    API Call for multiple Json Onject

--- APICall/app/src/main/java/com/example/apicall/MainActivity.java
+++ APICall/app/src/main/java/com/example/apicall/MainActivity.java
@@ -2,30 +2,45 @@ package com.example.apicall;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    //Single Object
     private static final String API_URL = "https://jsonplaceholder.typicode.com/posts/1";
 
+    //Multiple Objects
+    private static final String API_URL_1 = "https://jsonplaceholder.typicode.com/posts";
+
     TextView tv1 , tv2;
+    ListView l1;
+    List<String> title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv1 = findViewById(R.id.Text);
+        /*-----------------Viewing Single Object Only------------*/
+        /*tv1 = findViewById(R.id.Text);
         tv2 = findViewById(R.id.Text2);
 
 
@@ -46,7 +61,37 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Volley.newRequestQueue(this).add(jsonObject);
+        Volley.newRequestQueue(this).add(jsonObject);*/
+
+
+        /*------------------Viewing Multiple Object in List View------------------*/
+
+        l1 = findViewById(R.id.List);
+        title = new ArrayList<>();
+        JsonArrayRequest jsonArray = new JsonArrayRequest(Request.Method.GET, API_URL_1, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                for(int i=0; i<response.length() ; i++){
+                    try {
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        String titleResponse = jsonObject.getString("title");
+                        Log.d("API CALL: " , "On Response" + titleResponse );
+                        title.add(titleResponse);
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(MainActivity.this , android.R.layout.simple_list_item_1 , title);
+                l1.setAdapter(arrayAdapter);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "ERROR", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Volley.newRequestQueue(this).add(jsonArray);
 
     }
 }
